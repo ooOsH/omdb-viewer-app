2f854cde359ebba18eae6bb090fba74370063b66
ooOsH <github@nickwaller.co.uk>
2025-04-16 12:04:00 +0100

    💅 Style changes - Display and style recently viewed items

--- pages/recently-viewed.vue
+++ pages/recently-viewed.vue
@@ -1,7 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const store = useRecentlyViewedStore()
+
+const viewed = ref([])
+const showLoading = ref(true)
+
+onMounted(async () => {
+  viewed.value = store.data
+  showLoading.value = false
+})
+</script>
 
 <template>
   <section class="container mx-auto px-4 sm:px-6 lg:px-8">
     <h1 class="text-3xl font-bold mb-5">Recently Viewed</h1>
+    <div v-if="showLoading" class="text-lg text-gray-500">Loading...</div>
+    <div
+      v-if="viewed.length"
+      class="grid grid-cols-2 md:grid-cols-5 gap-4 mb-10"
+    >
+      <div v-for="item in viewed" :key="item.imdbID" class="md:hover:scale-105">
+        <NuxtLink :to="`/${item?.Type}/${item?.imdbID}`">
+          <img
+            :src="
+              item?.Poster !== 'N/A'
+                ? item?.Poster
+                : 'https://picsum.photos/300/450'
+            "
+            :alt="item?.Title"
+            class="w-full h-auto rounded"
+          />
+          <h3>{{ item?.Title }}</h3>
+        </NuxtLink>
+      </div>
+    </div>
   </section>
 </template>
